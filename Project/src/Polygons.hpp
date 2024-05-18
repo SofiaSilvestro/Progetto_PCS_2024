01dadf36ab74524e5489cb2e3cdf5046e28e7677
Giulia Calabrese <s294808@studenti.polito.it>
2024-05-18 15:08:38 +0200

    aggiunta di un po' di commenti

--- Project/src/Polygons.hpp
+++ Project/src/Polygons.hpp
@@ -10,18 +10,38 @@ using namespace Eigen;
 namespace FracturesLib{
 
 struct Fractures{
+
+    // Variabile che tiene traccia del numero di fratture
     unsigned int NumberFractures = 0;
+
+    // Identificatore univoco per ogni frattura
     unsigned int Id;
+
+    // Mappa che associa un id di frattura a una matrice di vertici
+    // La matrice ha 3 righe(coord x, y,z) e un numero dinamico di colonne (il numero dei vertici è variabile)
     map<unsigned int, Matrix<double, 3, Dynamic>> Vertices;
+
+    // Mappa che associa un id di frattura a un array di 4 elementi di tipo double (che rappresentano a,b,c,d dell'eq. del piano ax+by+cz+d=0 su cui è appoggiato il poligono)
     map<unsigned int, array<double,4>> Piano;
 };
 
 struct Traces{
+
+    // Identificatore univoco per ogni traccia
     unsigned int Id;
+
+    // Mappa che associa un id di traccia a un array di due id di frattura.
     map<unsigned int, array<unsigned int, 2>> FracturesId;
+
+    // Mappa che associa un id di traccia a un array di due vettori tridimensionali (che rappresentano le coordinate dei 2 vertici della traccia)
     map<unsigned int, array<Vector3d,2>> Vertices;
-    map<unsigned int, array<bool,2>> Tips;
+
+    // Mappa che associa un id di traccia a un array di due valori booleani (che ci dicono se la traccia è passante [false] oppure non-passante [true]) per ciascuno delle due fratture che definiscono la traccia)
     // il primo bool per il poligono FracturesId[i][0] e il secondo per FracturesId[i][1]
+    map<unsigned int, array<bool,2>> Tips;
+
+    // Array che indica la lunghezza delle tracce
+    array<double> Lenght;
 };
 
 }
--- Project/src/Utils.hpp
+++ Project/src/Utils.hpp
@@ -5,11 +5,47 @@ using namespace std;
 
 namespace FracturesLib{
 
+///La funzione importazione legge i dati da un file e popola una struttura Fractures con questi dati
+//Apre un file di testo
+//Legge il numero di fratture presenti nel file e lo memorizza in frattura.NumberFractures
+//Per ogni frattura:
+    //Legge l'id della frattura e il numero di vertici
+    //Legge le coordinate dei vertici e le memorizza in una matrice
+    //Aggiunge la matrice dei vertici alla mappa Vertices della struttura frattura
+//Calcola i coefficienti del piano per ciascuna frattura utilizzando tre dei suoi vertici e memorizza questi coefficienti nella mappa Piano della struttura frattura
+//Chiude il file e ritorna true se l'operazione è avvenuta con successo, false altrimenti
 bool importazione(const string& filename, Fractures& frattura);
+
+//La funzione distanza_al_quadrato calcola la distanza euclidea al quadrato tra due vettori tridimensionali v1 e v2
 double distanza_al_quadrato(Vector3d& v1, Vector3d& v2);
+
+///La funzione esportazione salva i dati delle tracce e delle fratture in un file di testo chiamato Traces.txt
+//Il file Traces.txt viene aperto in modalità di scrittura. Se il file non può essere aperto, viene visualizzato un messaggio di errore e la funzione termina
+//Scrive nel file il numero totale di tracce
+//Per ogni traccia, scrive l'ID della traccia, gli ID delle due fratture associate e le coordinate dei due vertici che definiscono la traccia
+//Crea una mappa (frattura_traccia) che associa l'ID di ogni frattura al numero di tracce che la coinvolgono
+//Per ogni frattura, se ha almeno una traccia associata, scrive l'ID della frattura e il numero di tracce che la coinvolgono
+//Per ogni traccia associata alla frattura, scrive l'ID della traccia, se è passante o no e la lunghezza della traccia
 void esportazione(Traces& traccia, Fractures& frattura);
+
+///La funzione valuta_intersezione determina se due fratture potrebbero intersecarsi basandosi su una verifica geometrica preliminare
+//Per entrambe le fratture, si calcola il baricentro sommando le coordinate di tutti i vertici e dividendo per il numero totale di vertici
+//Calcolo dei raggi:
+    //Si calcolano le distanze al quadrato tra ogni vertice e il baricentro per entrambe le fratture
+    //Si determinano i raggi prendendo la distanza massima tra i vertici e il baricentro per ciascuna frattura
+//Si verifica se la distanza al quadrato tra i due baricentri è inferiore o uguale alla somma dei raggi delle due sfere al quadrato
+    //Se questo è vero le fratture potrebbero intersecarsi, ma se questo è falso sicuramente non si intersecano
 bool valuta_intersezione (Fractures& frattura, unsigned int& Id1, unsigned int& Id2);
+
+///La funzione Retta_tra_piani restituisce un array di 6 elementi che descrive la retta di intersezione tra due fratture
+// I primi tre elementi dell'array coord_retta rappresentano la direzione della retta di intersezione e vengono calcolati come il prodotto vettoriale delle normali dei due piani
+// Risoluzione sistema lineare A*x=b con A matrice costruita con i coefficienti dei piani e la direzione della retta e b contenente i termini costanti delle equazioni dei piani e uno zero, trovando così un punto che appartiene alla retta intersecante
 array<double,6> Retta_tra_piani(Fractures& frattura, unsigned int& id1, unsigned int& id2);
+
+///La funzione Retta_per_due_vertici_della_frattura calcola la retta passante per due vertici di una frattura
+// Calcolo della direzione della retta (x2 - x1, y2 - y1, z2 - z1)
+//Calcolo del punto P sulla retta (x1, y1, z1)
 array<double,6> Retta_per_due_vertici_della_frattura(Fractures& frattura, unsigned int& id, unsigned int& i,unsigned int& j);
+
 Vector2d alpha_di_intersezione(array<double,6> r_intersez,array<double,6> r_fratt);
 }
